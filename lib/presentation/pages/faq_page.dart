b876d924444297185c5600e920e1dd0182072c66
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 21:51:22 +0630

    Update faq_page.dart

--- lib/presentation/pages/faq_page.dart
+++ lib/presentation/pages/faq_page.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
-class FAQPage extends StatefulWidget {
+class FAQPage extends StatelessWidget {
   const FAQPage({super.key});
 
   @override
-  State<FAQPage> createState() => _FAQPageState();
-}
-
-class _FAQPageState extends State<FAQPage> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Frequently Asked Questions")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Q: How can I book an appointment?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Text(
+              "A: Tap the Appointment button and fill in the required information.\n",
+            ),
+
+            Text(
+              "Q: What are your office hours?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Text("A: Monday - Friday, 9:00 AM to 5:00 PM\n"),
+
+            Text(
+              "Q: Can I cancel or reschedule my appointment?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Text(
+              "A: Yes, you can cancel or reschedule from the Appointment page.\n",
+            ),
+
+            Text(
+              "Q: Do you send notifications for upcoming appointments?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Text(
+              "A: Yes, you will receive notifications before your scheduled time.",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
