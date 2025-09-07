6c5b9b78685545084922666ccb619eea3d38a79d
Eaindra <eaindra68.hh@gmail.com>
2025-09-08 01:44:25 +0630

    Update ui_constant.dart

--- lib/config/ui_constant.dart
+++ lib/config/ui_constant.dart
@@ -3,11 +3,6 @@ import 'package:intl/intl.dart';
 class UIConstants {
   static const double horizontalPadding = 16.0;
 
-  static final dateFormat = DateFormat('dd/MMM/yyyy');
+  static final dateFormat = DateFormat('dd MMM yyyy');
   static final datetimeFormat = DateFormat('dd/MMM/yyyy h:mm a');
-
-  static final orderTimeFormat = DateFormat('dd MMM h:mm a');
-  static final timeFormat = DateFormat('h:mm a');
-  static final nearDateFormat = DateFormat(r"E, MMM d");
-  static final nearDateTimeFormat = DateFormat(r"E, MMM d h:mm a");
 }
