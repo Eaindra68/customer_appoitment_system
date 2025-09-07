5792c3b49cd7953c90da97c343d80ebcf4dc911b
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 22:35:06 +0630

    Create customer_take_image.dart

---
+++ lib/presentation/widgets/customer_take_image.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+
+class KYCFaceCard extends StatelessWidget {
+  final double height;
+  final double width;
+  final String url;
+  final Widget? icon;
+  final Uint8List? image;
+  final Function(File, Uint8List) onPicked;
+  final Function(BuildContext, Function(File, Uint8List)) showImagePicker;
+
+  const KYCFaceCard({
+    super.key,
+    required this.url,
+    required this.height,
+    required this.width,
+    this.icon,
+    required this.image,
+    required this.onPicked,
+    required this.showImagePicker,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => showImagePicker(context, onPicked),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).shadowColor.withValues(alpha: 0.03),
+                ),
+              ],
+              border: BoxBorder.all(color: Colors.black),
+              image: image == null
+                  ? null
+                  : DecorationImage(
+                      image: MemoryImage(image!),
+                      fit: BoxFit.cover,
+                    ),
+            ),
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                if (image == null)
+                  Positioned.fill(
+                    child:
+                        icon ??
+                        Icon(
+                          Icons.person_outline_rounded,
+                          color: Colors.white,
+                          size: min(height, width) * 0.8,
+                        ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
