0d12c077c769c7489ade12e24ea2e1aab3d2de79
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 18:29:08 +0630

    Update colors_constant.dart

--- lib/config/colors_constant.dart
+++ lib/config/colors_constant.dart
@@ -4,4 +4,8 @@ class ColorConst {
   ColorConst._();
 
   static const Color primary = Color(0xFFF37021);
+  static const Color fieldborderCol = Color(0xFF2A2A2A);
+  static const Color fieldtextCol = Color(0xFF575757);
+  static const Color iconCol = Color(0xFF656565);
+  static const Color labelCol = Color(0xFF979797);
 }
