6a685023e15cb2d8643ddb4aa42c221f60917f34
Eaindra <eaindra68.hh@gmail.com>
2025-09-08 01:44:35 +0630

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:ace_customer_appoitment_system/config/my_theme.dart';
 import 'package:flutter/material.dart';
-
+import 'package:hive_flutter/hive_flutter.dart';
 import 'presentation/pages/bottom_nav_page.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('customersBox');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
