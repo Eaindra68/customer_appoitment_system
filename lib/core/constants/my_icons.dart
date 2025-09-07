6ca3ff87c4715b47f31c44202e44dd1457ccf131
Eaindra <eaindra68.hh@gmail.com>
2025-09-08 01:44:27 +0630

    Update my_icons.dart

--- lib/core/constants/my_icons.dart
+++ lib/core/constants/my_icons.dart
@@ -1,6 +1,6 @@
 class MyIcons {
   MyIcons._();
 
-  static const calendarSvg = 'assets/icons/calendar.svg';
+  static const calendarSvg = 'assets/icons/calendar_outlined.svg';
   static const cameraSvg = 'assets/icons/camera.svg';
 }
