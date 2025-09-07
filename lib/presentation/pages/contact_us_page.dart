032506c378d3eb504c881f8114a765896a2c157b
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 20:46:11 +0630

    Update contact_us_page.dart

--- lib/presentation/pages/contact_us_page.dart
+++ lib/presentation/pages/contact_us_page.dart
@@ -1,8 +1,7 @@
 import 'package:ace_customer_appoitment_system/config/colors_constant.dart';
 import 'package:ace_customer_appoitment_system/core/extensions/context_exts.dart';
+import 'package:ace_customer_appoitment_system/presentation/widgets/home_card_widget.dart';
 import 'package:flutter/material.dart';
-
-import '../widgets/home_card_widget.dart';
 
 class ContactUsPage extends StatelessWidget {
   const ContactUsPage({super.key});
@@ -10,52 +9,41 @@ class ContactUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(62),
-        child: AppBar(
-          elevation: 2,
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          flexibleSpace: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.white,
-                    width: context.screenWidth,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Welcome to",
-                                style: context.textTheme.bodyLarge?.copyWith(
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Text(
-                                "Our Customer appointment system",
-                                style: context.textTheme.bodyMedium,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Text("Eng"),
-                        Icon(Icons.arrow_drop_down),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        backgroundColor: ColorConst.primary,
+        leading: InkWell(
+          onTap: () => context.pop(),
+          child: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          "Contact Us",
+          style: context.textTheme.titleLarge?.copyWith(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: HomeCardWidget(
+              icon: Icons.group,
+              label: 'Our Address',
+              onTap: () {},
             ),
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+            children: [
+              HomeCardWidget(icon: Icons.info, label: 'Aboou Us', onTap: () {}),
+              HomeCardWidget(
+                icon: Icons.person,
+                label: 'Contact Us',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
