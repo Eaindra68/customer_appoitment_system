32a519200aa548f41b71a08412ab8a4f5f18029d
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 18:01:24 +0630

    Create context_exts.dart

---
+++ lib/core/extensions/context_exts.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+extension ContextExts on BuildContext {
+  ThemeData get theme => Theme.of(this);
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+  Color get scaffoldBackgroundColor => Theme.of(this).scaffoldBackgroundColor;
+
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  Size get size => MediaQuery.of(this).size;
+  double get screenWidth => size.width;
+  double get screenHeight => size.height;
+  double get statusBarHeight => MediaQuery.of(this).padding.top;
+
+  Future<T?> pushWidget<T>(Widget screen, {Object? extra}) async {
+    return await Navigator.of(
+      this,
+    ).push(CupertinoPageRoute(builder: (context) => screen));
+  }
+
+  Future<T?> replaceWidget<T>(Widget screen, {Object? extra}) async {
+    return await Navigator.of(
+      this,
+    ).pushReplacement(MaterialPageRoute(builder: (context) => screen));
+  }
+
+  Future<T?> pushAndRemoveAll<T>(Widget screen) async {
+    if (!mounted) return null;
+    return await Navigator.pushAndRemoveUntil(
+      this,
+      MaterialPageRoute(builder: (context) => screen),
+      (route) => false,
+    );
+  }
+
+  void pop<T extends Object?>([T? result]) {
+    if (Navigator.canPop(this)) {
+      Navigator.pop<T>(this, result);
+    }
+  }
+}
