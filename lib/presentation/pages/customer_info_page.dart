35b0c50bf38e34cf3770487da986c649d93f7409
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 19:53:28 +0630

    Update customer_info_page.dart

--- lib/presentation/pages/customer_info_page.dart
+++ lib/presentation/pages/customer_info_page.dart
@@ -2,6 +2,8 @@ import 'package:ace_customer_appoitment_system/core/extensions/context_exts.dart
 import 'package:flutter/material.dart';
 
 import '../../config/colors_constant.dart';
+import '../widgets/birthday_selector_card.dart';
+import '../widgets/gender_selector_card.dart';
 
 class CustomerInfoPage extends StatefulWidget {
   const CustomerInfoPage({super.key});
@@ -11,10 +13,14 @@ class CustomerInfoPage extends StatefulWidget {
 }
 
 class _CustomerInfoPageState extends State<CustomerInfoPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  DateTime? _selectedDateOfBirth;
+  String? _selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[200],
       appBar: AppBar(
         backgroundColor: ColorConst.primary,
         leading: InkWell(
@@ -28,12 +34,64 @@ class _CustomerInfoPageState extends State<CustomerInfoPage> {
           style: context.textTheme.titleLarge?.copyWith(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pushWidget(CustomerInfoPage()),
-        shape: const CircleBorder(),
-        backgroundColor: Colors.red,
-        foregroundColor: Colors.white,
-        child: Icon(Icons.add),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: BoxBorder.all(color: Colors.black),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: "Name",
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                labelText: "Phone",
+                contentPadding: const EdgeInsets.symmetric(vertical: 12),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GenderSelector(
+              selectedGender: _selectedGender,
+              onChanged: (value) {
+                _selectedGender = value;
+                if (mounted) setState(() {});
+              },
+              isDisabled: false,
+            ),
+            BirthDateSelectorCard(
+              label: "Date of Birth",
+              selectedDate: _selectedDateOfBirth,
+              onDateChanged: (value) {
+                _selectedDateOfBirth = value;
+                if (mounted) setState(() {});
+              },
+            ),
+            BirthDateSelectorCard(
+              label: "Appointment Date",
+              selectedDate: _selectedDateOfBirth,
+              onDateChanged: (value) {
+                _selectedDateOfBirth = value;
+                if (mounted) setState(() {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
