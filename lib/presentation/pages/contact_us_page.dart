c1d9475ba3c43142898ee4b3adfa441ba9a7e886
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 21:51:20 +0630

    Update contact_us_page.dart

--- lib/presentation/pages/contact_us_page.dart
+++ lib/presentation/pages/contact_us_page.dart
@@ -1,7 +1,8 @@
 import 'package:ace_customer_appoitment_system/config/colors_constant.dart';
 import 'package:ace_customer_appoitment_system/core/extensions/context_exts.dart';
-import 'package:ace_customer_appoitment_system/presentation/widgets/home_card_widget.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/contact_us_card.dart';
 
 class ContactUsPage extends StatelessWidget {
   const ContactUsPage({super.key});
@@ -9,6 +10,7 @@ class ContactUsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         backgroundColor: ColorConst.primary,
         leading: InkWell(
@@ -23,23 +25,35 @@ class ContactUsPage extends StatelessWidget {
         ),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: HomeCardWidget(
-              icon: Icons.group,
+            child: ContactUsCard(
+              icon: Icons.location_pin,
               label: 'Our Address',
+              sublabel:
+                  "Building (19), 4th floor, MICT Park, Hlaing Township Yangon",
               onTap: () {},
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
-
             children: [
-              HomeCardWidget(icon: Icons.info, label: 'Aboou Us', onTap: () {}),
-              HomeCardWidget(
-                icon: Icons.person,
-                label: 'Contact Us',
-                onTap: () {},
+              Expanded(
+                child: ContactUsCard(
+                  icon: Icons.email,
+                  label: 'Email Us',
+                  sublabel: "admin@aceplussolutions.com",
+                  onTap: () {},
+                ),
+              ),
+              Expanded(
+                child: ContactUsCard(
+                  icon: Icons.phone,
+                  label: 'Call Us',
+                  sublabel: "09 777005818",
+                  onTap: () {},
+                ),
               ),
             ],
           ),
