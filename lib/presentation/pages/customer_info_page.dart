bd46af43f90f7d77af575e91b6d5e97f7c8fe8f0
Eaindra <eaindra68.hh@gmail.com>
2025-09-07 18:29:14 +0630

    Update customer_info_page.dart

--- lib/presentation/pages/customer_info_page.dart
+++ lib/presentation/pages/customer_info_page.dart
@@ -1,4 +1,7 @@
+import 'package:ace_customer_appoitment_system/core/extensions/context_exts.dart';
 import 'package:flutter/material.dart';
+
+import '../../config/colors_constant.dart';
 
 class CustomerInfoPage extends StatefulWidget {
   const CustomerInfoPage({super.key});
@@ -10,6 +13,28 @@ class CustomerInfoPage extends StatefulWidget {
 class _CustomerInfoPageState extends State<CustomerInfoPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.amber[200]);
+    return Scaffold(
+      backgroundColor: Colors.amber[200],
+      appBar: AppBar(
+        backgroundColor: ColorConst.primary,
+        leading: InkWell(
+          onTap: () => context.pop(),
+          child: Icon(Icons.arrow_back, color: Colors.white),
+        ),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          "Customer Information",
+          style: context.textTheme.titleLarge?.copyWith(color: Colors.white),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pushWidget(CustomerInfoPage()),
+        shape: const CircleBorder(),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
